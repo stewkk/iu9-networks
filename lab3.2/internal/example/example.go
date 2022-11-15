e92dda101acdd9d449eb2a35c2c230ea2267e2fb
Starovoytov Alexandr <saniy200311@gmail.com>
2022-11-15 12:53:17 +0300

    Add unit-test target and example

---
+++ lab3.2/internal/example/example.go
@@ -0,0 +1,5 @@
+package example
+
+func sum(a, b int) int {
+	return a + b
+}
---
+++ lab3.2/internal/example/example_test.go
@@ -0,0 +1,19 @@
+package example
+
+import (
+	"testing"
+    "github.com/stretchr/testify/suite"
+    "github.com/stretchr/testify/require"
+)
+
+type ExampleTestSuite struct {
+    suite.Suite
+}
+
+func (suite *ExampleTestSuite) TestExample() {
+	require.Equal(suite.T(), 3, sum(1, 2))
+}
+
+func TestExampleTestSuite(t *testing.T) {
+    suite.Run(t, new(ExampleTestSuite))
+}
