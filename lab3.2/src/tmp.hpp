044cff5704909b4a99c0a8003fe02cf108171f55
Starovoytov Alexandr <saniy200311@gmail.com>
2022-11-15 11:17:00 +0300

    Remove cpp files

--- lab3.2/src/tmp.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TMP_H_
-#define TMP_H_
-
-
-
-#endif // TMP_H_
--- lab3.2/tests/test_simple.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-def test_basic(service):
-    service.make_request({
-        "aboba": "dadaya"
-    })
